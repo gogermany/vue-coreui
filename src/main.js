ff27050dc4d545f7bec58e9d7a816e4213407fad
Goh <Gohz@outlook.com>
2020-03-08 02:09:32 +0700

    Core UI Admin Template

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
+import "core-js/stable";
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import CoreuiVue from "@coreui/vue";
+import { iconsSet as icons } from "./assets/icons/icons.js";
 import store from "./store";
 
 Vue.config.productionTip = false;
+Vue.config.performance = true;
+Vue.use(CoreuiVue);
 
 new Vue({
   router,
   store,
+  icons,
   render: h => h(App)
 }).$mount("#app");
